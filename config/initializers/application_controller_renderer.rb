b6efe1e693400a2a28065f507578b81253eb3244
ryujihagiwara <dragon7103next@icloud.com>
2020-07-29 17:54:53 +0900

    [Finish]5-3 画像投稿機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7895c07c86ed87d73ee6a4a505c6e6a6a82be67561a96e11efec1d58147e49a5f39526f5ae433bab63c846037f61178d952191eba4574d40b36265126ef6ac56'
