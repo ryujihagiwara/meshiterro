1513f86389646e59021442ca82012792e6b9beaa
ryujihagiwara <dragon7103next@icloud.com>
2020-07-30 15:28:44 +0900

    [Finish]5-5 マイページ

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -25,6 +25,8 @@ class PostImagesController < ApplicationController
 
   def destroy
     @post_image=PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
 
   private
